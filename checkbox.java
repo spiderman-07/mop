f122b384746e67c9f799d15729613c1373936d2d
spiderman-07 <aadhithiya.work@gmail.com>
2026-04-26 21:05:04 +0530

    Update checkbox.java

--- checkbox.java
+++ checkbox.java
@@ -1,12 +1,12 @@
 package demo;
 
-import java.util.List;
-import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import java.time.Duration;
+import java.util.List;
 
-public class checkbox {
+public class objectcount {
     public static void main(String[] args) {
 
         // Setup EdgeDriver
@@ -15,29 +15,13 @@ public class checkbox {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
 
-        // Open HTML file
-        driver.get("file:///D:/new.html");
+        // Open website
+        driver.get("https://compsem24.web.app/");
 
-        // Find all checkboxes
-        List<WebElement> checkboxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
-        System.out.println("Total checkboxes: " + checkboxes.size());
+        // Find all <p> elements and count
+        List<WebElement> paragraphs = driver.findElements(By.xpath("//p"));
+        int num = paragraphs.size();
 
-        // Click every 4th checkbox
-        for (int i = 0; i < checkboxes.size(); i += 4) {
-            checkboxes.get(i).click();
-        }
-
-        // Count checked and unchecked
-        int checkedCount = 0, uncheckedCount = 0;
-        for (WebElement checkbox : checkboxes) {
-            if (checkbox.isSelected()) {
-                checkedCount++;
-            } else {
-                uncheckedCount++;
-            }
-        }
-
-        System.out.println("Selected checkboxes: " + checkedCount);
-        System.out.println("Unselected checkboxes: " + uncheckedCount);
+        System.out.println("The number of <p> elements present are: " + num);
     }
 }
